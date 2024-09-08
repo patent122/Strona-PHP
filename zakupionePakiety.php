958d3ce65323719f555d1e1300608d78ec2bf38c
Patryk <patrykpanek3@gmail.com>
2024-09-08 18:04:21 +0200

    Poprawa kodu

--- zakupionePakiety.php
+++ zakupionePakiety.php
@@ -1,36 +1,6 @@
 <?php
 	session_start();
 	require("funkcje.php");
-
-	if (isset($_POST['zakup']) && $uczenID != -1 && jestRola('uczen')) {
-		$idPakietu = $_POST['idPakietu'];
-		if (poleWymagane('idPakietu')) {
-			$polaczenie = polaczZBaza();
-			$sql = "INSERT INTO ZakupionePakiety (UczenID, PakietID) VALUES ($uczenID, $idPakietu);";
-			$polaczenie->query($sql);
-			$polaczenie->close();
-
-			$_SESSION['form_sent'] = true;
-			header("Location: zakupionePakiety.php?uczenID=$uczenID&success=1");
-			exit();
-		}
-	} else if (isset($_POST['usuwanie']) && $uczenID != -1 && jestRola('uczen')) {
-		$id = $_POST['id'];
-		$polaczenie = polaczZBaza();
-		$sql = "DELETE FROM ZakupionePakiety WHERE ID = $id;";
-		$polaczenie->query($sql);
-		$polaczenie->close();
-
-		$_SESSION['form_sent'] = true;
-		header("Location: zakupionePakiety.php?uczenID=$uczenID&success=2");
-		exit();
-	}
-
-	if (isset($_SESSION['form_sent'])) {
-		unset($_SESSION['form_sent']); 
-		header("Location: zakupionePakiety.php?uczenID=$uczenID");
-		exit();
-	}
 ?>
 
 <!doctype html>
@@ -41,21 +11,24 @@
     <meta name="description" content="">
     <meta name="author" content="Patryk Panek">
     <title>Nauka jazdy</title>
-	
+
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
 	<link href="bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="navbar.css">
     <link rel="stylesheet" href="footer.css">
+
   </head>
 
 <body>
 	<main>
-	<?php menu(); 
+
+<?php menu(); 
 
 	$uczenID = -1;
 	$imie = "";
 	$nazwisko = "";
-	if(jestRola('uczen')) {
+	if(jestRola('uczen'))
+	{
 		$uczenID = $_SESSION['userId'];
 		$sql = "SELECT Imie, Nazwisko FROM Uzytkownicy WHERE ID = $uczenID";
 		$polaczenie = polaczZBaza();
@@ -65,7 +38,9 @@
 			$nazwisko = $row["Nazwisko"];
 		}
 		$polaczenie->close();
-	} else if(jestRola('instruktor') && isset($_POST['uczenID'])) {
+	}
+	else if(jestRola('instruktor') && isset($_POST['uczenID']))
+	{
 		$uczenID = $_POST['uczenID'];
 		$sql = "SELECT Imie, Nazwisko FROM Uzytkownicy WHERE ID = $uczenID";
 		$polaczenie = polaczZBaza();
@@ -76,98 +51,119 @@
 		}
 		$polaczenie->close();
 	}
-	?>
+?>
 
-	<div class="container px-1 py-5">
-		<h2 class="pb-2 border-bottom">Zakupione pakiety ucznia: <?php echo "$imie $nazwisko"; ?></h2>
+<div class="container px-1 py-5">
+		<h2 class="pb-2 border-bottom">Zakupione pakiety ucznia: <?php print("$imie $nazwisko"); ?></h2>
 
-		<?php
+<?php
 
-		if (isset($_GET['success'])) {
-			if ($_GET['success'] == 1) {
-				echo '<div class="alert alert-success">Pakiet został pomyślnie zakupiony!</div>';
-			} else if ($_GET['success'] == 2) {
-				echo '<div class="alert alert-success">Pakiet został pomyślnie usunięty!</div>';
-			}
+	if(isset($_POST['zakup']) && $uczenID!=-1 && jestRola('uczen')) {
+		$idPakietu = $_POST['idPakietu'];
+		if(poleWymagane('idPakietu')) {
+			$polaczenie = polaczZBaza();
+
+			$sql = "INSERT INTO ZakupionePakiety (UczenID, PakietID) VALUES ($uczenID, $idPakietu);";
+
+			$polaczenie->query($sql);
+			$polaczenie->close();
 		}
-		?>
+	}
+	else if(isset($_POST['usuwanie']) && $uczenID!=-1 && jestRola('uczen')) {
+		$id = $_POST['id'];
+		$polaczenie = polaczZBaza();
+		$sql = "DELETE FROM ZakupionePakiety WHERE ID = $id;";
+		$polaczenie->query($sql);
+		$polaczenie->close();
+	}
 
-		<p>Lista zakupionych pakietów.</p>
+?>		
+		<p>This is a paragraph.</p>
 
-		<?php if (jestRola('uczen')) { ?>
-			<form action="zakupionePakietyDodawanie.php" method="post">
-				<input type="hidden" name="uczenID" value="<?php echo $uczenID; ?>">
-				<input class="btn btn-primary" type="submit" name="zakupPakietuDlaUcznia" value="Zakup pakiet">
-			</form>
-		<?php } ?>
-
+ <?php if(jestRola('uczen')) { ?>
+		<form action="zakupionePakietyDodawanie.php" method="post">
+			<input type="hidden" name="uczenID" value="<?php print("$uczenID"); ?>">
+			<input class="btn btn-primary" type="submit" name="zakupPakietuDlaUcznia" value="Zakup pakiet">
+		</form>
+ <?php } ?>
 		<div class="row g-4 py-5">
-			<table class="table table-striped table-hover">
-				<thead>
-					<tr>
-						<th>Data zakupu</th>
-						<th>Cena</th>
-						<th>Opis</th>
-						<th>Lekcje wyk./wszystkie</th>
-						<th>Status</th>
-						<th>Lekcje</th>
-						<?php if (jestRola('uczen')) { ?>
-							<th>Usuwanie</th>
-						<?php } ?>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-				$polaczenie = polaczZBaza();
 
-				$sql = "SELECT ZP.ID, ZP.DataZakupu, ZP.Status, IloscWykorzystanychLekcji(ZP.ID) AS 'Wykorzystane', P.IloscLekcji, P.Cena, P.Opis
-						FROM ZakupionePakiety ZP JOIN Pakiety P ON P.ID = ZP.PakietID
-						WHERE ZP.UczenID = $uczenID;";
-				$result = $polaczenie->query($sql);
+<table class="table table-striped table-hover">
+  <thead>
+    <tr>
+		<th>Data zakupu</th>
+		<th>Cena</th>
+		<th>Opis</th>
+		<th>Lekcje wyk./wszystkie</th>
+		<th>Status</th>
+		<th>Lekcje</th>
+		 <?php if(jestRola('uczen')) { ?>
+		<th>Usuwanie</th>
+		<?php } ?>
+    </tr>
+  </thead>
 
-				while ($row = $result->fetch_assoc()) {
-					$id = $row["ID"];
-					$dataZakupu = $row["DataZakupu"];
-					$status = $row["Status"];
-					$wykorzystane = $row["Wykorzystane"];
-					$iloscLekcji = $row["IloscLekcji"];
-					$cena = $row["Cena"];
-					$opis = $row["Opis"];
-					?> 
-					<tr>
-						<td><?php echo $dataZakupu; ?></td>
-						<td><?php echo $cena; ?></td>
-						<td><?php echo $opis; ?></td>
-						<td><?php echo "$wykorzystane / $iloscLekcji"; ?></td>
-						<td><?php echo $status; ?></td>
-						<td>
-							<form action="lekcje.php" method="post">
-								<input type="hidden" name="pakietID" value="<?php echo $id; ?>">
-								<input type="hidden" name="uczenID" value="<?php echo $uczenID; ?>">
-								<input class="btn btn-primary" type="submit" name="lekcje" value="Lekcje">
-							</form>
-						</td>
-						<td>
-							<?php if (jestRola('uczen')) { ?>
-								<form action="zakupionePakiety.php" method="post">
-									<input type="hidden" name="id" value="<?php echo $id; ?>">
-									<input type="hidden" name="uczenID" value="<?php echo $uczenID; ?>">
-									<input class="btn btn-danger" type="submit" name="usuwanie" value="Usuń">
-								</form>
-							<?php } ?>
-						</td>
-					</tr>  
-				<?php
-				}
-				$polaczenie->close();
-				?>
-				</tbody>
-			</table>
-		</div>
+  <tbody>
+
+<?php
+
+$polaczenie = polaczZBaza();
+
+$sql = "SELECT ZP.ID, ZP.DataZakupu, ZP.Status, IloscWykorzystanychLekcji(ZP.ID) AS 'Wykorzystane', P.IloscLekcji,P.Cena, P.Opis
+FROM ZakupionePakiety ZP JOIN Pakiety P ON P.ID = ZP.PakietID
+WHERE ZP.UczenID = $uczenID;";
+
+$result = $polaczenie->query($sql);
+
+
+while ($row = $result->fetch_assoc()) {
+
+$id = $row["ID"];
+$dataZakupu = $row["DataZakupu"];
+$status = $row["Status"];
+
+$wykorzystane = $row["Wykorzystane"];
+$iloscLekcji = $row["IloscLekcji"];
+$cena = $row["Cena"];
+$opis = $row["Opis"];
+?> 
+	<tr>
+		<td><?php print("$dataZakupu"); ?></td>
+		<td><?php print("$cena"); ?></td>
+		<td><?php print("$opis"); ?></td>
+		<td><?php print("$wykorzystane / $iloscLekcji"); ?></td>
+		<td><?php print("$status"); ?></td>
+
+		<td>
+			<form action="lekcje.php" method="post">
+				<input type="hidden" name="pakietID" value="<?php print("$id"); ?>">
+				<input type="hidden" name="uczenID" value="<?php print("$uczenID"); ?>">
+				<input class="btn btn-primary" type="submit" name="lekcje" value="Lekcje">
+			</form>
+		</td>
+		<td>
+		<?php if(jestRola('uczen')) { ?>
+			<form action="zakupionePakiety.php" method="post">
+				<input type="hidden" name="id" value="<?php print("$id"); ?>">
+				<input type="hidden" name="uczenID" value="<?php print("$uczenID"); ?>">
+				<input class="btn btn-danger" type="submit" name="usuwanie" value="Usun">
+			</form>
+			<?php } ?>
+		</td>
+	</tr>  
+
+<?php
+   }
+$polaczenie->close();
+?>
+
+  </tbody>
+</table>
+</div>
 	</div>
-</main>
 
-<?php displayFooter(); ?>
+
+</main>
 
 </body>
 </html>
